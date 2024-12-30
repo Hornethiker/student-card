5a10353ca32e54b57845c942af50010501c88492
Hornethiker <amalpmjob@gmail.com>
2024-12-31 03:02:20 +0530

    Delete _nuxt directory

--- _nuxt/CafQchyX.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./Clji-vwv.js";import{_ as s,v as o,x as r,t as a}from"./DZLNLnul.js";const c={},n={class:"h-[100vh] w-full flex justify-center items-center bg-[#102236]"};function i(l,e){return a(),o("div",n,e[0]||(e[0]=[r("img",{src:t,alt:"Pedagogium Logo",class:"mx-auto mb-2"},null,-1)]))}const f=s(c,[["render",i]]);export{f as default};
